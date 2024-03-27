d4ad53aa65eee0fe619d91aa1eda77a42658c9b1
Max <max.jacob83@gmail.com>
2024-03-27 11:54:30 +0100

    initial commit

---
+++ stadtmeldeapp/src/test/java/com/stadtmeldeapp/StadtmeldeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stadtmeldeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StadtmeldeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
