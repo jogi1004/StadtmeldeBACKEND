2eba4de2256cb042a8776a6d29af0d5031860849
Joshua Reumann <grooviepie@gmx.de>
2024-03-30 14:48:18 +0900

    initial_commit_AdminView

--- stadtmeldeapp/src/main/java/com/stadtmeldeapp/StadtmeldeappApplication.java
+++ stadtmeldeapp/src/main/java/com/stadtmeldeapp/StadtmeldeappApplication.java
@@ -2,12 +2,21 @@ package com.stadtmeldeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 @SpringBootApplication
 public class StadtmeldeappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StadtmeldeappApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StadtmeldeappApplication.class, args);
+    }
 
+    @GetMapping("/")
+    public String index() {
+        return "index"; // This maps to index.html inside src/main/resources/templates
+    }
 }
+
+
