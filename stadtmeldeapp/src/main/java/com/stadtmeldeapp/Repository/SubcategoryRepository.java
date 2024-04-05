bb200a58da72a52293be46913e344042b14b051e
Max <max.jacob83@gmail.com>
2024-04-05 12:36:49 +0200

    methode gefixed
    Subcategories nach meincategorie_id zurückgeben geht jetzt auch

--- stadtmeldeapp/src/main/java/com/stadtmeldeapp/Repository/SubcategoryRepository.java
+++ stadtmeldeapp/src/main/java/com/stadtmeldeapp/Repository/SubcategoryRepository.java
@@ -1,10 +1,12 @@
 package com.stadtmeldeapp.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.stadtmeldeapp.Entity.SubcategoryEntity;
 
 public interface SubcategoryRepository extends JpaRepository<SubcategoryEntity, Integer> {
 
-    //List<SubcategoryEntity> findByMaincategoryId(int maincategoryId);
+    List<SubcategoryEntity> findByMaincategoryEntity_Id(int mainCategoryId);
 }
