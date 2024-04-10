dfe9b8895ef5274ff9dac78bf2d4d60da2a40f1c
Joshua Reumann <grooviepie@gmx.de>
2024-04-10 12:11:14 +0900

    categories_added

--- stadtmeldeapp/src/main/java/com/stadtmeldeapp/Controller/WebsiteController.java
+++ stadtmeldeapp/src/main/java/com/stadtmeldeapp/Controller/WebsiteController.java
@@ -1,19 +1,28 @@
 package com.stadtmeldeapp.Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.stadtmeldeapp.DTO.LoginDataDTO;
+import com.stadtmeldeapp.service.CategoryService;
 import com.stadtmeldeapp.service.JwtService;
+import com.stadtmeldeapp.service.ReportingLocationService;
 import com.stadtmeldeapp.service.UserDetailsServiceImpl;
+import com.stadtmeldeapp.Entity.MaincategoryEntity;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -33,6 +42,12 @@ public class WebsiteController {
 
     @Autowired
     private JwtService jwtService;
+
+    @Autowired
+    private CategoryService categoryService;
+    
+    @Autowired
+    private ReportingLocationService reportingLocationService;
 
     @GetMapping("/")
     public String index(HttpSession session, Model model) {
@@ -57,6 +72,20 @@ public class WebsiteController {
       session.setAttribute("token", token);
       session.setAttribute("test", "test");
       return "redirect:/";
+    }
+
+    @GetMapping("/mainWebsite/{id}")
+    public String getMainCategoryById(@PathVariable("id") int id, RedirectAttributes redirectAttributes,HttpSession session) {
+      logger.info("GET MAIN CATEGORY BY ID"); 
+      List<MaincategoryEntity> mainCategory = categoryService.getAllMainCategories();
+      // mainCategory = categoryService.getMainCategoryById(id);
+        if (mainCategory != null) {
+          logger.info("Main Category found."+ mainCategory.toString());
+          redirectAttributes.addFlashAttribute("MainCategories", mainCategory);
+          return "redirect:/"; 
+        }
+        logger.info("Main Category not found.");
+        return "redirect:/";
     }
     
 }
