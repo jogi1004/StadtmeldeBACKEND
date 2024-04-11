07d62a1fb719bc26904d3904b498cfc72c1fe2aa
Joshua Ginkel <joshua.ginkel@iese.fraunhofer.de>
2024-04-11 14:24:23 +0200

    Hotfix 3

--- stadtmeldeapp/src/main/java/com/stadtmeldeapp/config/DevConfig.java
+++ stadtmeldeapp/src/main/java/com/stadtmeldeapp/config/DevConfig.java
@@ -3,7 +3,6 @@ package com.stadtmeldeapp.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -11,7 +10,6 @@ import lombok.Setter;
 @Getter
 @Setter
 @Configuration
-@EnableJpaRepositories
 @ConfigurationProperties
 @Profile("dev")
 public class DevConfig {
