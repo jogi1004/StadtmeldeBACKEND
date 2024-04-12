ddcc7833b3b5da9b363a33a6de0172f42d5a7f0a
Joshua Ginkel <joshua.ginkel@iese.fraunhofer.de>
2024-04-12 12:22:47 +0200

    Hotfix #1

--- stadtmeldeapp/src/main/java/com/stadtmeldeapp/Repository/UserRepository.java
+++ stadtmeldeapp/src/main/java/com/stadtmeldeapp/Repository/UserRepository.java
@@ -3,11 +3,11 @@ package com.stadtmeldeapp.Repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.stadtmeldeapp.Entity.UserEntity;
 
-@Repository
+@EnableJpaRepositories
 public interface UserRepository extends JpaRepository<UserEntity, Integer>{
     Optional<UserEntity> findByUsername(String username);
     boolean existsByUsername(String username);
