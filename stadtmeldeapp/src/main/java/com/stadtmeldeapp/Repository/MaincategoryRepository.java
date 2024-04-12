837e9d44cc9c3cb778564fe71e447a78c6990933
Joshua Ginkel <joshua.ginkel@iese.fraunhofer.de>
2024-04-12 12:52:57 +0200

    Hotfix #2

--- stadtmeldeapp/src/main/java/com/stadtmeldeapp/Repository/MaincategoryRepository.java
+++ stadtmeldeapp/src/main/java/com/stadtmeldeapp/Repository/MaincategoryRepository.java
@@ -1,6 +1,7 @@
 package com.stadtmeldeapp.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.stadtmeldeapp.Entity.MaincategoryEntity;
 
@@ -8,6 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 
+@Repository
 public interface MaincategoryRepository extends JpaRepository<MaincategoryEntity, Integer> {
     List<MaincategoryEntity> findByReportingLocationEntity_Name(String name);
 
--- stadtmeldeapp/src/main/java/com/stadtmeldeapp/Repository/StatusRepository.java
+++ stadtmeldeapp/src/main/java/com/stadtmeldeapp/Repository/StatusRepository.java
@@ -4,8 +4,11 @@ package com.stadtmeldeapp.Repository;
 import com.stadtmeldeapp.Entity.StatusEntity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface StatusRepository extends JpaRepository<StatusEntity, Integer>{
 List<StatusEntity> findByReportingLocationEntityId(int reportingLocationId);
     
