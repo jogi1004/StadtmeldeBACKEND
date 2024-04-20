d97cd77926b59159ba892a81a17b05643a58f735
Max <max.jacob83@gmail.com>
2024-04-20 17:02:26 +0200

    minimale Zoomstufe der map begrenz

--- stadtmeldeapp/src/main/resources/static/js/map.js
+++ stadtmeldeapp/src/main/resources/static/js/map.js
@@ -2,6 +2,7 @@ var map = L.map('map').setView([49.245408, 7.364850], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
+    minZoom: 2,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
